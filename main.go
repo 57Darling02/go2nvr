26ab3fd066003635088aa7e7ddae13a3d79eb3ea
ZYJiang <3144223912@qq.com>
2026-03-06 16:11:20 +0800

    change version to 1.1.1

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ import (
 
 func main() {
 	// version will be set later from -buildvcs info, this used only as fallback
-	app.Version = "1.1.0"
+	app.Version = "1.1.1"
 
 	type module struct {
 		name string
